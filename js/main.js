6b7e2a2e37ea6231638894fe73a14eb0af253956
tornike <zhizhiashvilitornike@gmail.com>
2021-03-05 23:48:13 +0400

    s

--- js/main.js
+++ js/main.js
@@ -1,15 +1,51 @@
 var currentPage = 0;
 
-$('.book')
-.on('click', '.active', nextPage)
-.on('click', '.flipped', prevPage);
+// $('.book')
+// .on('click', '.active', nextPage)
+// .on('click', '.flipped', prevPage);
 
-$('.book').hammer().on("swipeleft", nextPage);
-$('.book').hammer().on("swiperight", prevPage);
+// $('.book').hammer().on("swipeleft", nextPage);
+// $('.book').hammer().on("swiperight", prevPage);
 $(".right_dot").on("click",function(){
 
     nextPage()
 })
+$(".left_dot").on("click",function(){
+
+    prevPage()
+
+    if ($(".page").eq(0).hasClass("active")) {
+        
+        $(".scene").removeClass("activer")
+
+        $(".left_dot").removeClass("flipped")
+         $(".right_dot").removeClass("flipped")
+
+           setTimeout(function(){
+             $(".book_outsite_text").fadeIn(500);
+             },1000)
+
+    }
+})
+
+$(".book_link").on("click",function(e){
+    e.preventDefault()
+    nextPage()
+    $(".book_outsite_text").fadeOut(0);
+    $(".scene").addClass("activer")
+
+
+    setTimeout(function(){
+     $(".left_dot").addClass("flipped")
+    $(".right_dot").addClass("flipped")
+},700)
+
+
+})
+
+
+
+
 function prevPage() {
   
   $('.flipped')
